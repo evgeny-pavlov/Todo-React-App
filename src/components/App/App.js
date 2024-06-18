4a10ee9bef8161a86f778cbd5a65b279522bfbaa
Evgeny Pavlov <epavlov725@gmail.com>
2024-06-18 21:48:40 +0300

    Day-2 deleteTodo,toggleTodo,Syles,Icons

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoList from "../TodoList/TodoList";
+import DummyTodoService from "../../services/dummy-todo-service"
 
 const App = () => {
+  const service = new DummyTodoService();
+  const initTodos = service.getTodos();
+
+  const [todos, setTodos] = useState(initTodos);
+
+  const deleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  const toggleTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  };
+
   return (
     <>
-      <TodoList />
+      <TodoList todos={todos} deleteTodo={deleteTodo} toggleTodo={toggleTodo} />
     </>
   );
 }
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,18 +1,19 @@
 import React from "react";
 import TodoItem from "../TodoItem";
-import DummyTodoService from "../../services/dummy-todo-service"
+import './style.css';
 
-const TodoList = () => {
-    const service = new DummyTodoService();
-    const todos = service.getTodos();
+const TodoList = ({ todos, deleteTodo, toggleTodo }) => {
 
     return (
-        <ul>
-            {todos.map((todo) => (
-                <li key={todo.id}>
+        <ul className="todo-list list-group d-inline-flex ">
+            {todos.map((todo, index) => (
+                <li key={todo.id} className="list-group-item ">
                     <TodoItem
                         title={todo.title}
                         completed={todo.completed}
+                        index={index}
+                        deleteTodo={deleteTodo}
+                        toggleTodo={toggleTodo}
                     />
                 </li>
             ))}
